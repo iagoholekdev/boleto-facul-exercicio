9e24529f5eba76fcee90215c7e9499d0e6d4640f
iagoholekdev <augustinhocarrara43@gmail.com>
2023-08-11 00:56:50 -0300

    mdfeature: criado testes unitários e ajustes classe main

--- src/main/java/org/projetoFacul/App.java
+++ src/main/java/org/projetoFacul/App.java
@@ -11,21 +11,29 @@ public class App
 {
     public static void main( String[] args )
     {
+        try {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o valor do boleto");
         double valorBoleto = scanner.nextDouble();
         System.out.println("Quanto você deseja pagar?");
         double valorPago = scanner.nextDouble();
 
-        Boleto boleto = new Boleto();
-        boleto
-                .setValorBoleto(valorBoleto)
-                .setValorPago(valorPago)
-                .setVencido(false);
+        Boleto boleto = alimentaBoleto(valorPago, valorBoleto);
         System.out.println(boleto);
 
-        BoletoController  boletoController = new BoletoController();
+        BoletoController boletoController = new BoletoController();
         System.out.println(boletoController.boletoController(boleto));
-
+        } catch (Exception e) {
+           System.out.println("Ocorreu um erro! Motivo: " + e.getMessage());
+        }
     }
+
+    public static Boleto alimentaBoleto(double valor, double valorBoleto) {
+        Boleto boleto = new Boleto();
+        return boleto
+                .setValorBoleto(valorBoleto)
+                .setValorPago(valor)
+                .setVencido(false);
+    }
+
 }
--- src/test/java/org/projetoFacul/AppTest.java
+++ src/test/java/org/projetoFacul/AppTest.java
@@ -1,8 +1,11 @@
 package org.projetoFacul;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.projetoFacul.models.Boleto;
+import org.projetoFacul.controller.BoletoController;
 
 /**
  * Unit test for simple App.
@@ -34,5 +37,37 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    String makeTest(Boleto boleto, double valor, double valorBoleto, boolean vencido){
+
+        boleto
+                .setValorBoleto(valorBoleto)
+                .setValorPago(valor)
+                .setVencido(vencido);
+        BoletoController boletoController = new BoletoController();
+        return (boletoController.boletoController(boleto));
+    }
+    public void testMain() {
+        Boleto boleto = new Boleto();
+        Assert.assertEquals( "Valor do boleto inconsistente!", makeTest(boleto,100, -100, true));
+        Assert.assertEquals( "Valor do boleto inconsistente!", makeTest(boleto,100, 0, true) );
+        Assert.assertEquals("Valor pago é zero!", makeTest(boleto,0, 100, true));
+        Assert.assertEquals("Valor pago é maior que o valor do boleto!", makeTest(boleto,200, 100, true) );
+        Assert.assertEquals("Boleto já passou da data de pagamento!", makeTest(boleto,100, 100, true));
+        Assert.assertEquals("Boleto quitado, você pagou o valor total!", makeTest(boleto,100, 100, false));
+        Assert.assertEquals("Boleto pago parcialmente, valor restante 50.00", makeTest(boleto,50, 100, false));
+
+    }
+
+    public void testAlimentaBoleto() {
+        Boleto boleto = new Boleto();
+        boleto
+            .setValorBoleto(100)
+            .setValorPago(50)
+            .setVencido(true);
+        Assert.assertTrue(boleto.getVencido());
+        Assert.assertEquals(100.00, boleto.getValorBoleto());
+        Assert.assertEquals(50.00, boleto.getValorPago());
     }
 }
