17c9c0d922cee75a2bb6d6e51b5e98a3391923e7
iagoholekdev <augustinhocarrara43@gmail.com>
2023-08-11 02:16:33 -0300

    mdfix4 - ajuste validações

--- src/main/java/org/projetoFacul/App.java
+++ src/main/java/org/projetoFacul/App.java
@@ -12,17 +12,17 @@ public class App
     public static void main( String[] args )
     {
         try {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o valor do boleto");
-        double valorBoleto = scanner.nextDouble();
-        System.out.println("Quanto você deseja pagar?");
-        double valorPago = scanner.nextDouble();
+          Scanner scanner = new Scanner(System.in);
+          System.out.println("Digite o valor do boleto");
+          double valorBoleto = scanner.nextDouble();
+          System.out.println("Quanto você deseja pagar?");
+          double valorPago = scanner.nextDouble();
 
-        Boleto boleto = alimentaBoleto(valorPago, valorBoleto);
-        System.out.println(boleto);
+          Boleto boleto = alimentaBoleto(valorPago, valorBoleto);
+          System.out.println(boleto);
 
-        BoletoController boletoController = new BoletoController();
-        System.out.println(boletoController.boletoController(boleto));
+          BoletoController boletoController = new BoletoController();
+          System.out.println(boletoController.boletoController(boleto));
         } catch (Exception e) {
            System.out.println("Ocorreu um erro! Motivo: " + e.getMessage());
         }
